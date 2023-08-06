bc172606590aa4e15a392dbc97c38643e7e41dff
diego <diegoaguilar221202@gmail.com>
2023-08-06 18:36:57 -0400

    se ajusta la url del socket

--- comunes/scripts.php
+++ comunes/scripts.php
@@ -9,9 +9,8 @@
 
 <script type="text/javascript" src="datatables/DataTables-1.12.1/js/jquery.dataTables.min.js"></script>
 
-<script type="text/javascript" src="js/socket.js"></script>
-<script type="text/javascript" src="js/contenido.js"></script>
-
 <script type="text/javascript">
 	var url = "127.0.0.1:12345";
 </script>
+<script type="text/javascript" src="js/socket.js"></script>
+<script type="text/javascript" src="js/contenido.js"></script>
