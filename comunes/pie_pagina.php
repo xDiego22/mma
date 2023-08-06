a5474c3b2cc746013e82c4c776b24d5ed3860257
diego <diegoaguilar221202@gmail.com>
2023-08-06 16:48:47 -0400

    se eliminan por desuso

--- comunes/pie_pagina.php
+++
@@ -1,5 +0,0 @@
-<div class="footer mt-auto py-9" style=" background:#191E21;padding: -100px;;position:fixed;bottom:0;width:100%">
-    <div class="text-light text-center">
-        &reg;Copyright 2022  Informatica 3302 - Cirez Barriga - Diego Aguilar - Luis Perdomo - Wong Ho - Ruander Cuello.
-    </div>
-</div>
