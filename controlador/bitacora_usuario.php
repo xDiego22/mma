30844baa91b5acdd91d4285006b9a9690b670409
diego <diegoaguilar221202@gmail.com>
2023-08-16 14:06:28 -0400

    se coloca accion de consulta para datatable

--- controlador/bitacora_usuario.php
+++ controlador/bitacora_usuario.php
@@ -23,12 +23,16 @@
 
 		$modulo = 8;
 
-		if(isset($_POST['accion'])){				
-			echo $objeto->consultar($rol_usuario);		
-			exit;
+		if(isset($_POST['accion'])){
+			$accion = $_POST['accion'];
+			
+			if($accion=='consultar'){
+
+				echo $objeto->consultar($rol_usuario);		
+				exit;
+			}
 		}
 
-		$listaconsulta = $objeto->consultar($rol_usuario);
 		$permisos = $objeto->permisos($rol_usuario);
 
 		if($permisos[0] == "true"){
