f4b1b45a21cfc2cf9388f8980ff7bba8b314cfac
diego <diegoaguilar221202@gmail.com>
2023-08-19 17:16:45 -0400

    se añaden valores para comprobaciones del captcha

--- controlador/inicio_sesion.php
+++ controlador/inicio_sesion.php
@@ -4,7 +4,7 @@
 
 	if(is_file("vista/".$pagina.".php")){
  
-		$objeto = new inicio_sesion(); //variable que toma en memoria todos los metodos de la clase inicio_sesion en Modelo 
+		$objeto = new inicio_sesion();
 
 		if(isset($_POST['accion_inicio_sesion'])){
 			
@@ -14,55 +14,72 @@
 				
 				$cedula = $objeto->busca_cedula();
 				$contraseña_encontrada = $objeto->busca_contrasena();
+				
+				//lo que viene de la vista
+				$captcha = $_POST['g-recaptcha-response'];
+				//llave secreta del captcha
+				$secretKeyCaptcha = '6LcEU7wnAAAAABojeM9Y4v4b3Jh1Vz7g998kCqPU';
+				
+				//respuesta de google
+				$respuestaCaptcha = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKeyCaptcha&response=$captcha");
 
-				if(isset($_COOKIE["block".$cedula])){
-					$mensaje = "El usuario ha sido bloqueado por 1 minuto";
-				}else{
+				$atributosRespuesta = json_decode($respuestaCaptcha,TRUE);
+				
+				if($atributosRespuesta['success']){
 
-					if(preg_match_all('/^[A-Za-z0-9ñÑ_.@$!%*?&#\/\b-]{6,70}$/',$_POST['contrasena_inicio'])){
-	
-						$verifica=password_verify($_POST['contrasena_inicio'],$contraseña_encontrada);
-						//condicion para saber si coinciden los datos correctos
-						if ($cedula!="Error en datos ingresados" and $verifica==1) {
-	
-							$rol = $objeto->busca_id_rol();
-	
-							$nombre = $objeto->busca_nombre();
-	
-							session_start();
-							$_SESSION['cedula'] = $cedula;
-							$_SESSION['rol'] = $rol;
-							$_SESSION['nombre'] = $nombre;
-	
-							$objeto->set_rol($rol);
-	
-							$modulo = $objeto->busca_modulo();
-							
-							$_SESSION['modulo'] = $modulo;
-							
-							$mensaje= "ok";//el ok e para el envio ajax
-						} 
-						//si no coinciden manda el mensaje de datos incorrectos
-						else{
-	
-							if (isset($_COOKIE["$cedula"])){
-								$contador = $_COOKIE["$cedula"];
-								$contador++;
-								setcookie($cedula,$contador,time()+ 120);
-	
-								if($contador >= 4){
-									setcookie("block".$cedula,$contador,time()+ 60);
-								}
-	
-							}else{
-								setcookie($cedula,1,time()+120);
-							}
-							$mensaje = "Datos Incorrectos";//mensaje que mostrara si no existe usuario registrado
-						}
+					if(isset($_COOKIE["block".$cedula])){
+						$mensaje = "El usuario ha sido bloqueado por 1 minuto";
 					}else{
-						$mensaje = 'ingrese contraseña correctamente';
+
+						if(preg_match_all('/^[A-Za-z0-9ñÑ_.@$!%*?&#\/\b-]{6,70}$/',$_POST['contrasena_inicio'])){
+		
+							$verifica=password_verify($_POST['contrasena_inicio'],$contraseña_encontrada);
+							//condicion para saber si coinciden los datos correctos
+							if ($cedula!="Error en datos ingresados" and $verifica==1) {
+		
+								$rol = $objeto->busca_id_rol();
+		
+								$nombre = $objeto->busca_nombre();
+		
+								session_start();
+								$_SESSION['cedula'] = $cedula;
+								$_SESSION['rol'] = $rol;
+								$_SESSION['nombre'] = $nombre;
+		
+								$objeto->set_rol($rol);
+		
+								$modulo = $objeto->busca_modulo();
+								
+								$_SESSION['modulo'] = $modulo;
+								
+								$mensaje= "ok";//el ok e para el envio ajax
+							} 
+							//si no coinciden manda el mensaje de datos incorrectos
+							else{
+		
+								if (isset($_COOKIE["$cedula"])){
+									$contador = $_COOKIE["$cedula"];
+									$contador++;
+									setcookie($cedula,$contador,time()+ 120);
+		
+									if($contador >= 4){
+										setcookie("block".$cedula,$contador,time()+ 60);
+									}
+		
+								}else{
+									setcookie($cedula,1,time()+120);
+								}
+								$mensaje = "Datos Incorrectos";//mensaje que mostrara si no existe usuario registrado
+							}
+						}else{
+							$mensaje = 'ingrese contraseña correctamente';
+						}
 					}
+
+				}else{
+					$mensaje = 'Error al comprobar el Captcha';
 				}
+
 			}else{
 				$mensaje = 'ingrese cedula correctamente';
 			}
