216b9c497292a005b7c7629ca9aaa726234d5ac4
diego <diegoaguilar221202@gmail.com>
2023-08-19 17:56:57 -0400

    se ajusta redireccionamiento

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		if(empty($_SESSION) and $pagina != 'inicio_sesion'){
 			session_start();
 		}
-		if(!isset($_SESSION['cedula']) || $_SESSION['cedula'] == "" and $pagina != 'inicio_sesion' and $pagina != 'error404'){
+		if(!isset($_SESSION['cedula']) || $_SESSION['cedula'] == "" and $pagina != 'inicio_sesion' and $pagina != 'error404' and $pagina != 'cambiar_contrasena' and $pagina != 'recuperar_contrasena'){
 			
 			require_once("controlador/cerrar_sesion.php");
 			exit;
