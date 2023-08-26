67169d1300b45bc9ccc180509627d0134a191dde
diego <diegoaguilar221202@gmail.com>
2023-08-26 19:00:21 -0400

    se ajusta la vista de la tabla

--- vista/gestionar_atleta.php
+++ vista/gestionar_atleta.php
@@ -27,7 +27,7 @@
 
 				<div class="container-fluid border my-4 pt-4 shadow bg-white rounded" style="width:95%;">
 			
-					<div class="container-fluid mt-4 mb-3">
+					<div class="mt-4 mb-3">
 						<div class="row justify-content-between">
 							<div class="col-auto mr-auto mb-2">
 								<div class="h4 text-dark">Gestionar Atletas</div>
@@ -46,63 +46,63 @@
 						</div>
 					</div>
 						
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col-md-12" >
-								<div class="table-responsive">
-									<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
-										<thead class="thead-dark">
-											<th>Club</th>
-											<th style="display:none">id_club</th> 
-											<th>foto</th>
-											<th>Cedula</th>
-											<th>Nombre</th>
-											<th>Apellido</th>
-											<th>Peso</th>
-											<th>Estatura</th>
-											<th>F. nacimiento</th>
-											<th>Telefono</th>
-											<th>Sexo</th>
-											<th>Deporte</th>
-											<th>Categoria</th>
-											<th>F. Ingreso</th>
-											<th>Entrenador</th>
-											<th>Acciones</th>
-										</thead>
-										<tbody id="resultadoconsulta">
-											<?php 
-												if(!empty ($listaconsulta)){
-													echo $listaconsulta;
-												}else{
-											?>
-												<tr>
-													<td colspan="15">No hay informacion</td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>			
-												</tr>
-											<?php 
-												}
-											?>
-										</tbody>
-									</table>
-								</div>
-								
+					
+					<div class="row">
+						<div class="col-md-12" >
+							<div class="table-responsive">
+								<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
+									<thead class="thead-dark">
+										<th>Club</th>
+										<th style="display:none">id_club</th> 
+										<th>foto</th>
+										<th>Cedula</th>
+										<th>Nombre</th>
+										<th>Apellido</th>
+										<th>Peso</th>
+										<th>Estatura</th>
+										<th>F. nacimiento</th>
+										<th>Telefono</th>
+										<th>Sexo</th>
+										<th>Deporte</th>
+										<th>Categoria</th>
+										<th>F. Ingreso</th>
+										<th>Entrenador</th>
+										<th>Acciones</th>
+									</thead>
+									<tbody id="resultadoconsulta">
+										<?php 
+											if(!empty ($listaconsulta)){
+												echo $listaconsulta;
+											}else{
+										?>
+											<tr>
+												<td colspan="15">No hay informacion</td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>			
+											</tr>
+										<?php 
+											}
+										?>
+									</tbody>
+								</table>
 							</div>
+							
 						</div>
 					</div>
+					
 					
 				</div>
 
