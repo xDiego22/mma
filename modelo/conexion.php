f0601022eb2a0f0096db939d3f1204d30073145f
diego <diegoaguilar221202@gmail.com>
2023-08-26 17:54:08 -0400

    se elimina bd vieja

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -27,18 +27,36 @@ class conexion{
         return $pdo;
     }
 
-    public function registrar_bitacora($cedula, $accion, $id_modulo)
+    protected function registrar_bitacora($cedula, $accion, $id_modulo)
     {
         $sql = "INSERT INTO bitacora_usuario (cedula_usuario, id_modulo, fecha_registro, hora_registro, accion_realizada) 
         VALUES(:ced, :id, CURDATE(), CURTIME(), :accion)";
 
         $stmt = $this->conecta()->prepare($sql);
 
+        $accion = $this->encriptar($accion);
+
         $stmt->execute(array(
             ":ced" => $cedula,
             ":id" => $id_modulo,
             ":accion" => $accion
         ));
+    }
+
+    protected function encriptar($texto){
+        $publicKey = openssl_pkey_get_public(file_get_contents('keys/public.key'));
+
+        openssl_public_encrypt($texto,$textoEncriptado,$publicKey);
+
+        return base64_encode($textoEncriptado);
+    }
+
+    protected function desencriptar($texto){
+        $privateKey = openssl_pkey_get_private(file_get_contents('keys/private.key'));
+
+        openssl_private_decrypt(base64_decode($texto),$textoDesencriptado,$privateKey);
+
+        return $textoDesencriptado;
     }
 		
 }
