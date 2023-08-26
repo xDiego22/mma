5700166a4405e307d39f6ebdf5da255e69e1ecd6
diego <diegoaguilar221202@gmail.com>
2023-08-26 19:36:51 -0400

    se ajusta lista y se muestra la direccion

--- vista/gestionar_eventos.php
+++ vista/gestionar_eventos.php
@@ -27,7 +27,7 @@
 				<?php require_once('comunes/menu-topbar.php')?>
 				<div class="container-fluid border my-4 shadow bg-white rounded" style="width:95%;">
 			
-					<div class="container-fluid mt-4 mb-3">
+					<div class="mt-4 mb-3">
 						<div class="row">
 							<div class="col-auto mr-auto mb-2">
 								<div class="h4 text-dark">Gestionar Eventos</div>
@@ -46,52 +46,50 @@
 						</div>
 					</div>
 						
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col-md-12" >
-								<div class="table-responsive">
-									<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
-										<thead class="thead-dark">
-											<th style="display:none;">id_club</th>
-											<th>Nombre</th>
-											<th>Fecha</th>
-											<th>Hora</th>
-											<th>Monto</th>
-											<th>Responsable</th>
-											<th style="display:none;">Direccion</th>
-											<th>Juez 1</th>
-											<th>Juez 2</th>
-											<th>Juez 3</th>
-											<th>Acciones</th>
-											
-										</thead>
-										<tbody id="resultadoconsulta">
-											<?php 
-												if(!empty ($listaconsulta)){
-													echo $listaconsulta;
-												}else{
-											?>
-												<tr>
-													<td colspan="9">No hay informacion</td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-													<td style="display:none;"></td>
-												</tr>
-											<?php 
-												}
-											?>
-										</tbody>
-									</table>
-								</div>
-								
+					<div class="row">
+						<div class="col-md-12" >
+							<div class="table-responsive">
+								<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
+									<thead class="thead-dark">
+										<th style="display:none;">id_club</th>
+										<th>Nombre</th>
+										<th>Fecha</th>
+										<th>Hora</th>
+										<th>Monto</th>
+										<th>Responsable</th>
+										<th>Direccion</th>
+										<th>Juez 1</th>
+										<th>Juez 2</th>
+										<th>Juez 3</th>
+										<th>Acciones</th>
+										
+									</thead>
+									<tbody id="resultadoconsulta">
+										<?php 
+											if(!empty ($listaconsulta)){
+												echo $listaconsulta;
+											}else{
+										?>
+											<tr>
+												<td colspan="9">No hay informacion</td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+												<td style="display:none;"></td>
+											</tr>
+										<?php 
+											}
+										?>
+									</tbody>
+								</table>
 							</div>
+							
 						</div>
 					</div>
 					
