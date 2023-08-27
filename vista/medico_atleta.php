a4362f052b7b5852049f640d3620b21a0a2ab7d8
diego <diegoaguilar221202@gmail.com>
2023-08-26 21:47:03 -0400

    se ajusta lista

--- vista/medico_atleta.php
+++ vista/medico_atleta.php
@@ -28,7 +28,7 @@
 
 				<div class="container-fluid border my-4 shadow bg-white rounded" style="width:95%;">
 			
-					<div class="container-fluid mt-4 mb-3">
+					<div class="mt-4 mb-3">
 						<div class="row">
 							<div class="col-auto mr-auto mb-2">
 								<div class="h4 text-dark">Gestionar Informacion Medica de Atletas</div>
@@ -47,54 +47,52 @@
 						</div>
 					</div>
 						
-					<div class="container-fluid">
-						<div class="row">
-							<div class="col-md-12" >
-								<div class="table-responsive">
-									<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
-										<thead class="thead-dark">
-											<th style="display:none;">id_atleta</th>
-											<th>Cedula</th>
-											<th>Nombre atleta</th>
-											<th>Medicamento que consume</th>
-											<th>Enfermedad que padece</th>
-											<th>Discapacidad que posee</th>
-											<th>Dieta alimenticia</th>
-											<th>Enfermedades pasadas</th>
-											<th>Nombre de emergencia</th>
-											<th>Tlf. de emergencia</th>
-											<th>Tipo de relacion</th>
-											<th>Acciones</th>
-										</thead>
-										<tbody id="resultadoconsulta">
-											<?php 
-												if(!empty ($listaconsulta)){
-													echo $listaconsulta;
-												}else{
-											?>
-												<tr>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td colspan="11">No hay informacion</td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-													<td style="display:none"></td>
-												</tr>
-												
-											<?php 
-												}
-											?>
-										</tbody>
-									</table>
-								</div>
-								
+					<div class="row">
+						<div class="col-md-12" >
+							<div class="table-responsive">
+								<table class="table table-striped table-hover table-borderless" id="tablaconsulta" width="100%" cellspacing="0">
+									<thead class="thead-dark">
+										<th style="display:none;">id_atleta</th>
+										<th>Cedula</th>
+										<th>Nombre atleta</th>
+										<th>Medicamento que consume</th>
+										<th>Enfermedad que padece</th>
+										<th>Discapacidad que posee</th>
+										<th>Dieta alimenticia</th>
+										<th>Enfermedades pasadas</th>
+										<th>Nombre de emergencia</th>
+										<th>Tlf. de emergencia</th>
+										<th>Tipo de relacion</th>
+										<th>Acciones</th>
+									</thead>
+									<tbody id="resultadoconsulta">
+										<?php 
+											if(!empty ($listaconsulta)){
+												echo $listaconsulta;
+											}else{
+										?>
+											<tr>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td colspan="11">No hay informacion</td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+												<td style="display:none"></td>
+											</tr>
+											
+										<?php 
+											}
+										?>
+									</tbody>
+								</table>
 							</div>
+							
 						</div>
 					</div>
 					
