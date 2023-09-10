637991b5b198a9e836d3abd47c130960a9e9d867
diego <diegoaguilar221202@gmail.com>
2023-09-10 00:03:14 -0400

    pruebas unitarias ajustadas permisos

--- tests/roles/incorrectos/permisos_rolesTest.php
+++ tests/roles/incorrectos/permisos_rolesTest.php
@@ -16,7 +16,7 @@ class permisos_rolesTest extends TestCase{
         $this->rol->set_rol_2('dwed2');
 
         
-        $this->assertEquals('ingrese datos correctamente',$this->rol->actualizar_permisos('1','false','true','false','false','1'));
+        $this->assertEquals('ingrese datos correctamente',$this->rol->actualizar_permisos('3d43$%','d#%R&&:;','#$:)=','##$%:;¡','#E#!/($','1'));
     }
 }
 
--- tests/roles/vacios/permisos_rolesTest.php
+++ tests/roles/vacios/permisos_rolesTest.php
@@ -16,7 +16,7 @@ class permisos_rolesTest extends TestCase{
         $this->rol->set_rol_2('');
 
         
-        $this->assertEquals('ingrese datos correctamente',$this->rol->actualizar_permisos('1','false','true','false','false','1'));
+        $this->assertEquals('ingrese datos correctamente',$this->rol->actualizar_permisos('','','','','','1'));
     }
 }
 
