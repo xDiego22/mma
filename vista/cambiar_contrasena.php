bf5264c278f7f7ff6fdd5e2e4ee4da576d8d0169
diego <diegoaguilar221202@gmail.com>
2023-09-14 02:39:16 -0400

    maxlength modificado

--- vista/cambiar_contrasena.php
+++ vista/cambiar_contrasena.php
@@ -29,12 +29,12 @@
 
                 <div class="col-md-12 mb-4">
                     <label for="contrasena">Nueva contraseña</label>
-                    <input class="form-control" type="password" name="contrasena" id="contrasena" placeholder="Ej: &quot;15345987&quot;">
+                    <input class="form-control" maxlength="15" type="password" name="contrasena" id="contrasena" placeholder="Ej: &quot;15345987&quot;">
                     <span id="scontrasena" style="color: #ff0000;"></span>
                 </div>
                 <div class="col-md-12">
                     <label for="contrasena2">Confirmar contraseña</label>
-                    <input class="form-control" type="password" name="contrasena2" id="contrasena2" placeholder="Ej: &quot;15345987&quot;">
+                    <input class="form-control" maxlength="15" type="password" name="contrasena2" id="contrasena2" placeholder="Ej: &quot;15345987&quot;">
                     <span id="scontrasena2" style="color: #ff0000;"></span>
                 </div>
             </div>
