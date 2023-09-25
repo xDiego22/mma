767df602326358bb1ae6f00c9dec653c2136741e
diego <diegoaguilar221202@gmail.com>
2023-09-25 15:19:59 -0400

    cambios de nombre variable

--- controlador/generar_llaves.php
+++ controlador/generar_llaves.php
@@ -5,16 +5,15 @@
         "private_key_bits" => 2048,
         "default_md" => "sha256",
     );
-    /*generacion de llaves con los parametros establecidos*/
-    $generar = openssl_pkey_new($parametros);
+     /* generacion de llaves con los parametros establecidos */
+    $llaves_generadas = openssl_pkey_new($parametros);
 
-    //exportacion de la llave privada
-    openssl_pkey_export($generar,$privateKey,NULL,$parametros);
+    /* exportacion de la llave privada */
+    openssl_pkey_export($llaves_generadas, $privateKey, NULL, $parametros);
+    
+    /* exportacion de la llave publica */
+    $publicKey = openssl_pkey_get_details($llaves_generadas);
 
-    //exportacion de la llave publica
-    $publicKey = openssl_pkey_get_details($generar);
-
-    //guardado del archivo que contiene las llaves
-    file_put_contents('keys/private.key',$privateKey);
-    file_put_contents('keys/public.key',$publicKey['key']);
+    file_put_contents('keys/private2.key',$privateKey);
+    file_put_contents('keys/public2.key',$publicKey['key']);
 ?>
