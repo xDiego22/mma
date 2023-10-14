8b045ff7de9ddcccebdaa69d4838b94ce6313529
diego <diegoaguilar221202@gmail.com>
2023-10-13 23:08:12 -0400

    juste de funcion de comprobar si ya existen llaves

--- controlador/generar_llaves.php
+++ controlador/generar_llaves.php
@@ -1,7 +1,12 @@
 <?php 
-    /* parametros para generar las llaves*/
+    //comprobar si ya las llaves han sido creadas anteriormente
+    if (is_file('keys/private.key') && is_file('keys/public.key')){
+        echo "las llaves ya existen";
+        exit();
+    }
+    /* parametros de configuracion para generar las llaves*/
     $parametros = array(
-        "config" => "C:/xampp/php/extras/openssl/openssl.cnf",
+        "config" => "keys/openssl.cnf",
         "private_key_bits" => 2048,
         "default_md" => "sha256",
     );
@@ -14,6 +19,8 @@
     /* exportacion de la llave publica */
     $publicKey = openssl_pkey_get_details($llaves_generadas);
 
-    file_put_contents('keys/private2.key',$privateKey);
-    file_put_contents('keys/public2.key',$publicKey['key']);
+    file_put_contents('keys/private.key',$privateKey);
+    file_put_contents('keys/public.key',$publicKey['key']);
+
+    echo "llaves generadas";
 ?>
