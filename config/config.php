0ca3b1c39fb4dd38e9f4cd1a19526babe61ea8f0
diego <diegoaguilar221202@gmail.com>
2023-10-16 20:42:03 -0400

    try catch

--- config/config.php
+++ config/config.php
@@ -1,12 +1,16 @@
 <?php
-	//inicializacion de instancia env
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
+	try{
+        //inicializacion de instancia env
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
 
-    define('DB_HOST',$_ENV['DB_HOST']);
-    define('DB_NAME',$_ENV['DB_NAME']);
-    define('DB_USER',$_ENV['DB_USER']);
-    define('DB_PASSWORD',$_ENV['DB_PASSWORD']);
-    define('PATH_PUBLIC_KEY',$_ENV['PATH_PUBLIC_KEY']);
-    define('PATH_PRIVATE_KEY',$_ENV['PATH_PRIVATE_KEY']);
+        define('DB_HOST',$_ENV['DB_HOST']);
+        define('DB_NAME',$_ENV['DB_NAME']);
+        define('DB_USER',$_ENV['DB_USER']);
+        define('DB_PASSWORD',$_ENV['DB_PASSWORD']);
+        define('PATH_PUBLIC_KEY',$_ENV['PATH_PUBLIC_KEY']);
+        define('PATH_PRIVATE_KEY',$_ENV['PATH_PRIVATE_KEY']);}
+    catch (\Throwable $th) {
+        die('Error: no existe .env');
+    }
 ?>
