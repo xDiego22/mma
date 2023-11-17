fb81799c428f17ca82a38b4acc568a681c7fbd1b
Cebasmb <cirezeduardo10@gmail.com>
2023-11-17 19:32:34 -0400

    modelo actualizado

--- modelo/historial_atleta.php
+++ modelo/historial_atleta.php
@@ -1,77 +1,107 @@
-<?php 
+<?php
 namespace modelo;
 use modelo\conexion as conexion;
 use PDO;
 use Exception;
-class historial_atleta extends conexion{
 
-    public function mostrar_resultado($atleta){
-		if(preg_match_all('/^[0-9\b]{1,10}$/',$atleta)){	
-			$co = $this->conecta();
-			$co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+use flight;
 
-			try{
+class historial_atleta extends conexion {
 
-				$resultado = $co->prepare("SELECT b.nombre, GROUP_CONCAT(' ', a.ronda)from resultados as a, eventos as b where a.id_evento = b.id and a.atleta1 = '$atleta'");
+    public function mostrar_resultado($atleta) {
+        if (preg_match_all('/^[0-9\b]{1,10}$/', $atleta)) {
+            $co = $this->conecta();
+            $co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-				$resultado->execute();
+            try {
+                $resultado = $co->prepare("SELECT b.nombre, GROUP_CONCAT(' ', a.ronda) FROM resultados as a, eventos as b WHERE a.id_evento = b.id AND a.atleta1 = '$atleta'");
+                $resultado->execute();
 
-				if($resultado){
+                if ($resultado) {
+                    $respuesta = '';
+                    // Ciclo foreach se usa casi siempre para recorrer los resultados de las consultas
+                    foreach ($resultado as $r) {
+                        $respuesta .= "<tr>";
+                        $respuesta .= "<td>" . $r[0] . "</td>";
+                        $respuesta .= "<td>" . $r[1] . "</td>";
+                        $respuesta .= "</tr>";
+                    }
+                    return $respuesta;
+                } else {
+                    return 'vacio';
+                }
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+        } else {
+            return 'ingrese datos correctamente';
+        }
+    }
 
-					$respuesta = '';
-					//ciclo foreach se usa casi siempre para recorrer los resultados de las consultas
-					foreach($resultado as $r){ 
-						$respuesta = $respuesta."<tr>";
-								
-							$respuesta = $respuesta."<td>";
-								$respuesta = $respuesta.$r[0];
-							$respuesta = $respuesta."</td>";
+    public function consulta_atletas() {
+        $co = $this->conecta();
+        $co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-							$respuesta = $respuesta."<td>";
-								$respuesta = $respuesta.$r[1];
-							$respuesta = $respuesta."</td>";
+        try {
+            $resultado = $co->prepare("SELECT DISTINCT id, cedula, nombre FROM inscripcion_evento WHERE id_evento GROUP BY cedula");
+            $resultado->execute();
 
+            if ($resultado) {
+                $respuesta = '';
+                // Ciclo foreach se usa casi siempre para recorrer los resultados de las consultas
+                foreach ($resultado as $r) {
+                    $respuesta .= "<option value=" . $r['id'] . ">" . $r['cedula'] . " -- " . $r['nombre'] . "</option>";
+                }
+                return $respuesta;
+            } else {
+                return '';
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-						$respuesta = $respuesta."</tr>";
-					}
-					return $respuesta;
-				}
-				else {
-					return 'vacio';
-				}
+    /* ---- APP ---- */
 
-			}catch(Exception $e) {
-				return $e->getMessage();
-			}
-		}else{
-			return 'ingrese datos correctamente';
+    public function consultaApp() {
+        try {
+            $db = $this->conecta();
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $query = "SELECT DISTINCT id, cedula, nombre FROM inscripcion_evento GROUP BY cedula;";
+
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+
+            $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+public function mostrarApp() {
+	try {
+		$db = $this->conecta();
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$query = "SELECT e.nombre AS nombre_evento, GROUP_CONCAT(' ', r.ronda) AS resultado_ronda
+		FROM resultados AS r
+		LEFT JOIN eventos AS e ON r.id_evento = e.id 
+		WHERE r.atleta1 = :atleta
+		GROUP BY e.id";
+
+		$stmt = $db->prepare($query);
+		$stmt->execute();
+
+		$resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		Flight::json($resultados);
+
+	} catch (Exception $e) {
+		echo $e->getMessage();
 		}
 	}
-    public function consulta_atletas(){
 
-		$co = $this->conecta();
-		$co->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		try{
-
-			$resultado = $co->prepare("SELECT DISTINCT id, cedula, nombre from inscripcion_evento WHERE id_evento GROUP BY cedula");
-			$resultado->execute();
-			if($resultado){
-
-				$respuesta = '';
-				//ciclo foreach se usa casi siempre para recorrer los resultados de las consultas
-				foreach($resultado as $r){
-					$respuesta = $respuesta."<option value=".$r['id'].">".$r['cedula']." -- ".$r['nombre']."</option>";
-				}
-				return $respuesta;
-			}
-			else {
-				return '';
-			}
-
-		}catch(Exception $e) {
-			return $e->getMessage();
-		}
-	}
 }
 ?>
