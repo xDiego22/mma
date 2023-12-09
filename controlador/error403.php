3c31fb1143f8a481b7c9f1fdf8c0d547df91ffe3
diego <diegoaguilar221202@gmail.com>
2023-12-09 18:11:14 -0400

    error 403 en htacces

---
+++ controlador/error403.php
@@ -0,0 +1,7 @@
+<?php 
+
+    if(is_file("vista/error403.php")){
+        require_once("vista/error403.php");
+    }
+
+?>
---
+++ vista/error403.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php require_once('comunes/cabecera.php') ?>
+</head>
+<body id="page-top"> 
+
+	<div class="container-fluid">
+
+        
+        <div class="text-center">
+            <div class="error mx-auto" data-text="403">403</div>
+            <p class="lead text-gray-800 mb-5">Acceso Prohibido</p>
+        </div>
+
+    </div>
+
+    <script src="js/jquery.min.js" ></script>
+    <script src="js/popper.min.js" ></script>
+    <script src="js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
