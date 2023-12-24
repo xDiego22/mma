1dc1ac9828ccef63691b6618b8537eb508cdcdcb
diego <diegoaguilar221202@gmail.com>
2023-12-24 16:30:33 -0400

    test de consulta modificado

--- tests/usuarios/correctos/consultar_gestionar_usuariosTest.php
+++ tests/usuarios/correctos/consultar_gestionar_usuariosTest.php
@@ -1,27 +1,12 @@
 <?php 
 
 use PHPUnit\Framework\TestCase;
-
 use modelo\gestionar_usuarios;
-
 class consultar_gestionar_usuariosTest extends TestCase{
     private $usuarios;
 
     public function setUp():void{
         $this->usuarios = new gestionar_usuarios();
-
-        $this->usuarios->set_cedula_usuarios('7852258');
-        $this->usuarios->set_nombre_usuarios('victor gutierrez');
-        $this->usuarios->set_contrasena_usuarios('gutie123');
-        $this->usuarios->set_rol_usuario('1');
-        $this->usuarios->set_correo_usuarios('roswins12.11@outlook.com');
-
-        $this->usuarios->registrar('1','29831184','7');
-    }
-
-    public function tearDown():void {
-        $this->usuarios->set_cedula_usuarios('7852258');
-        $this->usuarios->eliminar('29831184','7','1'); 
     }
 
     public function testConsultarUsuarios(){
